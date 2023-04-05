7a1b7d9a66f95e8ea77968470a0d2495841d58a4
Anastasiia Tkachuk <anastaisha.580@gmail.com>
2023-04-05 19:47:19 +0300

    add entity and save first user in database

--- src/main/java/edu/AnastasiiaTkachuk/HibernateRunner.java
+++ src/main/java/edu/AnastasiiaTkachuk/HibernateRunner.java
@@ -1,5 +1,6 @@
 package edu.AnastasiiaTkachuk;
 
+import edu.AnastasiiaTkachuk.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -7,6 +8,7 @@ import org.hibernate.cfg.Configuration;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.concurrent.BlockingDeque;
 
 public class HibernateRunner {
@@ -17,11 +19,22 @@ public class HibernateRunner {
         //Connection connection = DriverManager.getConnection("db.url", "db.username", "db.password");  //Session
 
         Configuration configuration = new Configuration();
+        //configuration.addAnnotatedClass(User.class);
         configuration.configure();
 
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
              Session session = sessionFactory.openSession()) {
-            System.out.println("OK");
+
+            session.beginTransaction();
+            User user = User.builder()
+                    .username("test@gmail.com")
+                    .firstname("Ivan")
+                    .lastname("Ivanov")
+                    .birthday(LocalDate.of(2000, 1, 19))
+                    .age(23)
+                    .build();
+            session.persist(user);
+            session.getTransaction().commit();
         }
     }
 }
