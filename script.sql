6726d2672f33c01be6563ac7320936cf7a0107bc
Anastasiia Tkachuk <anastaisha.580@gmail.com>
2023-04-17 20:46:52 +0300

    modify script.sql

--- script.sql
+++ script.sql
@@ -9,6 +9,31 @@ CREATE TABLE users
     company_id INT REFERENCES company (id)
 );
 
+CREATE TABLE company_locale
+(
+    company_id INT NOT NULL REFERENCES company(id),
+    lang CHAR(2) NOT NULL,
+    description VARCHAR(128) NOT NULL,
+    PRIMARY KEY (company_id, lang)
+);
+
+CREATE TABLE users_chat
+(
+    id BIGSERIAL PRIMARY KEY ,
+    user_id BIGINT REFERENCES users(id),
+    chat_id BIGINT REFERENCES chat(id),
+    created_at TIMESTAMP NOT NULL ,
+    created_by VARCHAR(128) NOT NULL
+);
+
+DROP TABLE users_chat;
+
+CREATE TABLE chat
+(
+    id BIGSERIAL PRIMARY KEY ,
+    name VARCHAR(64) NOT NULL UNIQUE
+);
+
 CREATE TABLE profile
 (
     id BIGSERIAL PRIMARY KEY ,
