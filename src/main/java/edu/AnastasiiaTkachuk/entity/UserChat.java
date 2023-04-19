bbd1f00bd39cadd75d3b735db8bb9c0f0296a6e8
Anastasiia Tkachuk <anastaisha.580@gmail.com>
2023-04-19 15:36:57 +0300

    add MappedSuperclass

--- src/main/java/edu/AnastasiiaTkachuk/entity/UserChat.java
+++ src/main/java/edu/AnastasiiaTkachuk/entity/UserChat.java
@@ -14,11 +14,7 @@ import java.time.Instant;
 @Builder
 @Entity
 @Table(name = "users_chat")
-public class UserChat {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+public class UserChat extends BaseEntity<Long>{
     @ManyToOne
     private User user;
 
