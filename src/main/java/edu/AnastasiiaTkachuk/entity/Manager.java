e527691d765299cf7e6801fb1de9b43d0ca0aa79
Anastasiia Tkachuk <anastaisha.580@gmail.com>
2023-04-19 20:27:39 +0300

    add SIGNE_TABLE

--- src/main/java/edu/AnastasiiaTkachuk/entity/Manager.java
+++ src/main/java/edu/AnastasiiaTkachuk/entity/Manager.java
@@ -1,6 +1,7 @@
 package edu.AnastasiiaTkachuk.entity;
 
 
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,6 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@DiscriminatorValue("manager")
 public class Manager extends User{
     private String projectName;
 
--- src/main/java/edu/AnastasiiaTkachuk/entity/User.java
+++ src/main/java/edu/AnastasiiaTkachuk/entity/User.java
@@ -15,10 +15,11 @@ import java.util.Set;
 @ToString(exclude = {"company", "profile", "userChats"})
 @Entity
 @Table(name = "users")
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "type")
 public abstract class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private PersonalInfo personalInfo;
